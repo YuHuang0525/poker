e2ef7b2c86f151b7e15663586844027dab09c93b
Yu Huang <yu.huang2018@gmail.com>
2023-08-27 22:11:11 -0700

    :sparkles: shuffle cards and assign cards to three players' containers, also take out the last three cards

--- Game.java
+++ Game.java
@@ -33,6 +33,38 @@ public class Game {
 
     public static void main(String[] args) {
 
+        // shuffle all the cards
+        Collections.shuffle(allCards);
+        System.out.println("Cards after shuffling: " + allCards.toString());
+
+        System.out.println("================================================================");
+
+        // initialize players' containers
+        List<Card> p1 = new ArrayList<>();
+        List<Card> p2 = new ArrayList<>();
+        List<Card> p3 = new ArrayList<>();
+
+        // assign cards to the players
+        for (int i = 0; i < allCards.size()-3; i++) {
+            if (i % 3 == 0) {
+                p1.add(allCards.get(i));
+            } else if (i % 3 == 1) {
+                p2.add(allCards.get(i));
+            } else {
+                p3.add(allCards.get(i));
+            }
+        }
+
+        // take the last three cards in the deck
+        List<Card> lastThree = allCards.subList(allCards.size()-3, allCards.size());
+
+        // sort cards in the containers
+
+
+        System.out.println("Player1 cards: " + p1.toString());
+        System.out.println("Player2 cards: " + p2.toString());
+        System.out.println("Player3 cards: " + p3.toString());
+        System.out.println("Last three cards: " + lastThree.toString());
     }
 
 }
